2d89f2ceab221e3a8de468c764eb1ced557fde8c
Unknown <boveah@gmail.com>
2018-01-07 16:56:36 -0500

    Merge branch 'rpattack'

--- src/GUI/GameFrame.java
+++ src/GUI/GameFrame.java
@@ -18,8 +18,13 @@ public class GameFrame extends JFrame {
         JMenuBar bar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem exit = new JMenuItem("Exit");
+        JMenuItem settings = new JMenuItem("Settings");
+        JMenuItem player = new JMenuItem("Player Style");
+
         exit.addActionListener(e -> System.exit(0));
+        
         fileMenu.add(exit);
+        fileMenu.add(settings);
         bar.add(fileMenu);
         setJMenuBar(bar);
     }
